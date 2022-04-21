b766c7ae0ea745ac7c15014b24e9f9f051484e1c
haig <haigharutunian@protonmail.com>
2022-04-20 20:30:16 -0400

    Commits 2
    added notes file

--- src/com/company/notesOnly.java
+++ src/com/company/notesOnly.java
@@ -1,2 +1,18 @@
-package com.company;public class notesOnly {
-}
+/*
+in initialInputs instead of what i have i can do this instead
+public void readAandB(){
+        //You can use scanner to init your values
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a :");
+        a = scan.nextInt();
+        System.out.print("Enter b :");
+        b = scan.nextInt();
+    }
+from - https://stackoverflow.com/questions/41075769/using-variables-of-one-class-in-another-class-java
+so i can have strings inbetween the inputs.
+
+
+
+
+
+ */
