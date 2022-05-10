dcf79c25a1aead7d05f22365409f5cade01bc281
heath <ashaw4@umbc.edu>
2022-05-10 12:43:50 -0400

    Added JavaDocs

--- src/com/company/OutputExcel.java
+++ src/com/company/OutputExcel.java
@@ -14,7 +14,9 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
-
+/**This class exports the calculations derived from the other classes and sends them to an excel spreadsheet.
+ *
+ */
 public class OutputExcel {
 
 
